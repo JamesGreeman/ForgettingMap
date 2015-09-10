565d33fa807caa69999edb05a558e7bc1ad2eac8
jgreeman <jgreeman@192.168.1.73>
2015-09-10 22:52:13 +0100

    Improved JavaDoc

--- src/com/greeman/forgettingmap/ForgettingMap.java
+++ src/com/greeman/forgettingmap/ForgettingMap.java
@@ -9,7 +9,6 @@ import java.util.Map.Entry;
  * @author jgreeman
  * @param <K> class for key
  * @param <V> class for value
- * Will take an int for maximum size on construction, if this size is reached new items will replace the least used item.
  */
 public class ForgettingMap<K,V> {
     private final Map<K, V>         values      =   new HashMap<>();
@@ -17,7 +16,7 @@ public class ForgettingMap<K,V> {
     private final int               maxSize;
     
     /**
-     * Constructor for ForgettingMap
+     * Constructor for ForgettingMap.
      * @param maxSize the maximum size this map can be
      */
     public ForgettingMap(int maxSize) {
@@ -25,12 +24,12 @@ public class ForgettingMap<K,V> {
     }
     
     /**
-     * method to add a new association to the map
+     * Adds a new association to the map.
+     * If the map is full and does not contain the key being added the least accessed
+     * element is removed.  Regardless of this the new value is added under the key
+     * and an access count of 0 is set for the key.
      * @param key key to be added
      * @param value value to be added
-     * Will first remove the key (does nothing if it does not exists) not synchronized as the function has its own thread protection
-     * Next checks the size of the keys, if it is already at the max size then the least accessed is removed
-     * finally the new values are added
      */
     public void add(K key, V value){
         synchronized(this){
@@ -43,9 +42,11 @@ public class ForgettingMap<K,V> {
         }
     }
     /**
-     * method to get a value associated with a specific key
+     * Returns the value corresponding to a given key.
+     * If the map contains the key the access count is incremented and the value
+     * is returned, otherwise null is returned
      * @param key to be found
-     * @return a value if key exists or null
+     * @return corresponding value if exists else null
      */
     public V find(K key){
         synchronized(this){
@@ -58,17 +59,24 @@ public class ForgettingMap<K,V> {
         }
     }
     
-    public void removeLeastAccessed(){
+    /**
+     * Removes the least accessed element.
+     */
+    private void removeLeastAccessed(){
         synchronized(this){
             K toRemove  =   getLeastAccessed();
             if (toRemove != null){
-                values.remove(toRemove);
-                accessCount.remove(toRemove);
+                remove(toRemove);
             }
         }
     }
     
-    public K getLeastAccessed(){
+    /**
+     * Returns the key that has been accessed the least.
+     * Checks each entry in the access count, storing the smallest element.
+     * @return key corresponding to smallest element
+     */
+    private K getLeastAccessed(){
         synchronized(this){
             K key   =   null;
             Integer minimum =   Integer.MAX_VALUE;
@@ -81,6 +89,12 @@ public class ForgettingMap<K,V> {
             return key;
         }
     }
+    
+    /**
+     * Removes a specified key from the map.
+     * Removes both the value and the access count from the internal data structures
+     * @param key key to be removed
+     */
     public void remove(K key){
         synchronized(this){
             values.remove(key);
@@ -89,9 +103,12 @@ public class ForgettingMap<K,V> {
     }
     
     /**
-     * method to get the size of the map
-     * @return the size
-     * used the keys list to get the size, sizes of the array are checked here, if there is a problem a runtime error is thrown
+     * Returns the size of the map.
+     * The method will check the size of internal data structures are in line and
+     * that the maximum size parameter has not been exceeded.  If it either of these
+     * are in error then a BadSizeException is throw.  Otherwise the size of the values
+     * map is returned.
+     * @return the size of values map
      */
     public int size() throws BadSizeException{
         synchronized(this){
